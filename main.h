66afdfcac45faccc93a5cfd10ad202145c0e067c
Onojo Ameh <amehonoj@gmail.com>
2022-11-24 11:32:49 -0800

    _printf.c

--- main.h
+++ main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_buf(char *str, int buff);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+unsigned int handl_buf(char *buf, char c, unsigned int buff_ind);
+int get_id_num(const char *s, int index);
 
 #endif
